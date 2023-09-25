debf308da87e72dc599934b3c2b1f5dcaff2b0c2
Saurabh Shukla <saurabh1258.be21@chitkarauniversity.edu.in>
2023-09-25 23:44:55 +0530

    rating-added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-unused-vars */
 import React from 'react'
+import { v4 as uuidv4 } from 'uuid'
 import Header from './components/Header'
 import FeedbackItem from './components/FeedbackItem'
 import { useState } from 'react'
@@ -7,28 +8,43 @@ import FeedbackList from './components/FeedbackList'
 import FeedbackData from './data/FeedbackData'
 import FeedbackStats from './components/FeedbackStats'
 import FeedbackForm from './components/FeedbackForm'
+import AboutPage from './pages/AboutPage'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 function App() {
     const [feedback, setFeedback] = useState(FeedbackData)
-    const deleteFeedback = (id) => { 
+    const addFeedback = (newFeedback) => {
+        newFeedback.id = uuidv4();
+        setFeedback([...feedback, newFeedback])
+    }
+    const deleteFeedback = (id) => {
         if (window.confirm('Are you sure you want to delete this feedback?')) {
-            console.log('App', id); 
+            console.log('App', id);
             setFeedback(feedback.filter((item) => item.id !== id))
         }
-   
+
     }
     return (
-        <>
+        <Router>
             <Header />
             <div className='container'>
-                <FeedbackForm/>
-                <FeedbackStats feedback={feedback} />
-                <FeedbackList
-                    feedback={feedback}
-                    handleDelete={deleteFeedback}
-                />
+                <Routes>
+                    <Route exact path='/' element={
+                        <>
+                            <FeedbackForm handleAdd={addFeedback} />
+                            <FeedbackStats feedback={feedback} />
+                            <FeedbackList
+                                feedback={feedback}
+                                handleDelete={deleteFeedback}
+                            />
+                        </>
+                    } >
+
+                    </Route>
+                    <Route path='/about' element={<AboutPage />} />
+                </Routes>
             </div>
 
-        </>
+        </Router>
     )
 }
 
---
+++ src/components/RatingSelect.jsx
@@ -0,0 +1,130 @@
+import React from 'react'
+import { useState } from 'react'
+function RatingSelect({select}) {
+    const [selected,setSelected] = useState(10);
+    const handleChange = (e) => {
+        setSelected(Number(e.currentTarget.value));
+        select(Number(e.currentTarget.value));
+    }
+  return (
+    <ul className="rating">
+        <li>
+            <input 
+                type="radio"
+                name="rating"
+                value="1"
+                id="num1"
+                checked={selected === 1}
+                onChange={handleChange}
+            />
+            <label htmlFor="num1">1</label>
+        </li>
+        <li>
+            <input
+                type="radio"
+                name="rating"
+                value="2"
+                id="num2"
+                checked={selected === 2}
+                onChange={handleChange}
+            />
+            <label htmlFor="num2">2</label>
+        </li>
+        <li>
+            <input
+                type="radio"
+                name="rating"
+                value="3"
+                id="num3"
+                checked={selected === 3}
+                onChange={handleChange}
+            />
+            <label htmlFor="num3">3</label>
+        </li>
+        <li>
+            <input
+                type="radio"
+                name="rating"
+                value="4"
+                id="num4"
+                checked={selected === 4}
+                onChange={handleChange}
+            />
+            <label htmlFor="num4">4</label>
+        </li>
+        <li>
+            <input
+                type="radio"
+                name="rating"
+                value="5"
+                id="num5"
+                checked={selected === 5}
+                onChange={handleChange}
+            />
+            <label htmlFor="num5">5</label>
+        </li>
+        <li>
+            <input
+                type="radio"
+                name="rating"
+                value="6"
+                id="num6"
+                checked={selected === 6}
+                onChange={handleChange}
+            />
+            <label htmlFor="num6">6</label>
+        </li>
+        <li>
+            <input
+                type="radio"
+                name="rating"
+                value="7"
+                id="num7"
+                checked={selected === 7}
+                onChange={handleChange}
+            />
+            <label htmlFor="num7">7</label>
+        </li>
+        <li>
+            <input
+                type="radio"
+                name="rating"
+                value="8"
+                id="num8"
+                checked={selected === 8}
+                onChange={handleChange}
+            />
+            <label htmlFor="num8">8</label>
+        </li>
+        <li>
+            <input
+                type="radio"
+                name="rating"
+                value="9"
+                id="num9"
+                checked={selected === 9}
+                onChange={handleChange}
+            />
+            <label htmlFor="num9">9</label>
+        </li>
+        <li>
+            <input
+                type="radio"
+                name="rating"
+                value="10"
+                id="num10"
+                checked={selected === 10}
+                onChange={handleChange}
+            />
+            <label htmlFor="num10">10</label>
+        </li>
+
+
+
+
+
+    </ul>
+  )
+}
+
+export default RatingSelect
