8d21be1ce9ef1bade77b92a8cfd72712415169e1
Saurabh Shukla <saurabh1258.be21@chitkarauniversity.edu.in>
2023-10-02 02:18:38 +0530

    updated

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -4,26 +4,16 @@ import { v4 as uuidv4 } from "uuid";
 const FeedbackContext = createContext();
 
 export const FeedbackProvider = ({ children }) => {
-<<<<<<< HEAD
   const [feedbackEdit, setFeedbackEdit] = useState({
     item: {},
     edit: false
-=======
-  const [feedbackEdit , setFeedbackEdit] = useState({
-    item:{},
-    edit:false
->>>>>>> e311aec72fafde432d287d1a507febd2f7c07ddb
   })
   // eslint-disable-next-line no-unused-vars
   const [feedback, setFeedback] = useState([
     {
       id: 1,
       text: "This item is from context",
-<<<<<<< HEAD
       rating: 10
-=======
-      rating: 10,
->>>>>>> e311aec72fafde432d287d1a507febd2f7c07ddb
     },
   ]);
   const deleteFeedback = (id) => {
@@ -36,7 +26,6 @@ export const FeedbackProvider = ({ children }) => {
     newFeedback.id = uuidv4();
     setFeedback([...feedback, newFeedback]);
   };
-<<<<<<< HEAD
   const editFeedback = (item) => {
     setFeedbackEdit({
       item,
@@ -45,32 +34,15 @@ export const FeedbackProvider = ({ children }) => {
   }
   const updateFeedback = (id, updItem) => {
     setFeedback(feedback.map((item) => (item.id === id ? { ...item, ...updItem } : item)))
-=======
-  const editFeedback = (item) =>{
-    setFeedbackEdit({
-      item,
-      edit:true
-    })
-  }
-  const updateFeedback = (id,updItem)=>{
-    setFeedback(feedback.map((item)=>(item.id === id ? {...item,...updItem}:item)))
->>>>>>> e311aec72fafde432d287d1a507febd2f7c07ddb
   }
   return (
     <FeedbackContext.Provider
       value={{
         feedback,
-<<<<<<< HEAD
         feedbackEdit,
         deleteFeedback,
         addFeedback,
         editFeedback,
-=======
-        deleteFeedback,
-        addFeedback,
-        editFeedback,
-        feedbackEdit,
->>>>>>> e311aec72fafde432d287d1a507febd2f7c07ddb
         updateFeedback,
       }}
     >
